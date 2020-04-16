f1b8114f033ab62d5cdbbc6b767ddab6a2d9c264
Amit Biswas <biswas1992@hotmail.com>
2020-04-16 00:53:40 -0400

    Created custom alert

---
+++ githubexplorer/GFTitleLabel.swift
@@ -0,0 +1,37 @@
+//
+//  GFTitleLabel.swift
+//  githubexplorer
+//
+//  Created by Amit Biswas on 4/15/20.
+//  Copyright © 2020 Amit Biswas. All rights reserved.
+//
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+        configure()
+    }
+    
+    private func configure() {
+        textColor                 = .label
+        adjustsFontSizeToFitWidth  = true
+        minimumScaleFactor        = 0.9
+        lineBreakMode            = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+}
