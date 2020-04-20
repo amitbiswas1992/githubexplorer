8103cb13951a776314837c1214c160a0021fcdd1
Amit Biswas <biswas1992@hotmail.com>
2020-04-20 03:24:32 -0400

    Created User Model  and follower Model

---
+++ githubexplorer/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  githubexplorer
+//
+//  Created by Amit Biswas on 4/20/20.
+//  Copyright © 2020 Amit Biswas. All rights reserved.
+//
+
+import Foundation
+
+struct Followers: Codable {
+    var login: String
+    var avatarUrl: String
+}
