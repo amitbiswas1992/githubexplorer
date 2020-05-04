b2cd5b3005c640c79a82f7d6bcb25bbe53ac593d
Amit Biswas <biswas1992@hotmail.com>
2020-05-03 23:57:42 -0400

    FollowerCell created

---
+++ githubexplorer/Custom Views/Cell/FollowerCellCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FollowerCell.swift
+//  githubexplorer
+//
+//  Created by Amit Biswas on 5/3/20.
+//  Copyright © 2020 Amit Biswas. All rights reserved.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    
+    static let reuseID = "FollowerCell"
+    
+}
