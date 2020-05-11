9ae945af85544892cf5640a7390b31765f82bce3
Amit Biswas <biswas1992@hotmail.com>
2020-05-11 02:13:12 -0400

    Createed GFAvatarImageView

---
+++ githubexplorer/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,26 @@
+//
+//  GFAvatarImageView.swift
+//  githubexplorer
+//
+//  Created by Amit Biswas on 5/11/20.
+//  Copyright © 2020 Amit Biswas. All rights reserved.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        
+    }
+}
