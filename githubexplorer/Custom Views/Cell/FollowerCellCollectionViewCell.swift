63dacd642b3d7109040b65bfd3d335d1b3891838
Amit Biswas <biswas1992@hotmail.com>
2020-05-30 01:42:21 -0400

    Created custom follower cell

--- githubexplorer/Custom Views/Cell/FollowerCellCollectionViewCell.swift
+++ githubexplorer/Custom Views/Cell/FollowerCellCollectionViewCell.swift
@@ -23,4 +23,30 @@ class FollowerCell: UICollectionViewCell {
     required init?(coder: NSCoder) {
         fatalError("Init(Coder:) has not been implemented")
     }
+    
+    func set(follower: Follower) {
+        usernameLabel.text = follower.login
+    }
+    
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        let padding: CGFloat = 8
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
+            
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+            
+    
+        ])
+        
+    }
 }
